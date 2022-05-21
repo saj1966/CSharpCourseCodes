346ee20ba8ea6c6bfd4c64060e90e5501af94f9c
saj1966 <seljahmi@gmail.com>
2022-05-21 21:48:47 +0200

    First C# Program
    This is my first C# Program

---
+++ FirstProgram/FirstProgram/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstProgram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // This is my first program in C#
+            Console.WriteLine("Hello World");
+            Console.ReadKey();
+        }
+    }
+}
